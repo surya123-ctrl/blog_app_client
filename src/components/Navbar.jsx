f6a94399356d74ece7a0518aaa66d215c4896d11
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-10-31 02:08:05 +0530

    designed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">Blog-App</div>
+        <div className="link">
+          <Link className="link" to="?cat=art">
+            <h6>Art</h6>
+          </Link>
+          <Link className="link" to="?cat=science">
+            <h6>Science</h6>
+          </Link>
+          <Link className="link" to="?cat=technology">
+            <h6>Technology</h6>
+          </Link>
+          <Link className="link" to="?cat=cinema">
+            <h6>Cinema</h6>
+          </Link>
+          <Link className="link" to="?cat=design">
+            <h6>Design</h6>
+          </Link>
+          <Link className="link" to="?cat=food">
+            <h6>Food</h6>
+          </Link>
+          <span className="link">Surya</span>
+          <span className="link">Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Create Post
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
