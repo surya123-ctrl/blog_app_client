d981d6e33a6a8b3edcf0e058a8d4ab38ff4752ac
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-10-31 01:08:33 +0530

    created components and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
 import React from 'react'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Write from './pages/Write';
+import Home from './pages/Home';
+import SinglePage from './pages/SinglePage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/:id',
+        element: <SinglePage />,
+      },
+      {
+        path: '/write',
+        element: <Write />,
+      }
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  }
+]);
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div className='app'>
+      <div className='container'>
+        <RouterProvider router={router} />
+      </div>
     </div>
   )
 }
+
 
 export default App
