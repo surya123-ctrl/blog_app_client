a3416321fc96b75aabbc52a77727737005eee96e
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-10-31 02:13:18 +0530

    designed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="logo">Blog-App</div>
+      <span>
+        Made with ❤️ and <b>React.js</b>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
